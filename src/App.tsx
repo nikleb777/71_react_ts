9f0959b31cfce3f65dbb34103b202bebc8fc7279
nikleb777 <nikleb1998@gmail.com>
2025-11-10 19:45:47 +0100

    bis Lesson08

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// Импорты лекций
+// import Lesson06 from "./lessons/Lesson06/Lesson06";
+// import Lesson07 from "./lessons/Lesson07/Lesson07";
+import Lesson08 from "./lessons/Lesson08/Lesson08";
+
+// Импорты домашних работ
+// import Homework07 from "./homeworks/Homework07/Homework07";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* Лекция 6 - TypeScript */}
+      {/* <Lesson06 /> */}
+
+      {/* Лекция 7 - TypeScript: object types */}
+      {/* <Lesson07 /> */}
+      {/* <Homework07/> */}
+
+      {/* Лекция 8 - Styling components */}
+      <Lesson08 />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/lessons/Lesson06/Lesson06.tsx
@@ -0,0 +1,84 @@
+import "./styles.css";
+
+function Lesson06() {
+  // Типы переменных
+  // Тип переменных указывается после название через двоеточие
+  // 1. Типизация строк - string
+  let userName: string = "Mary";
+  userName = `Tom`;
+  // userName = true;
+  let result: string = 1 + 2 + "4";
+
+  // 2. Типизация чисел - number
+  let simpleNumber: number = 23;
+  simpleNumber = Infinity;
+  simpleNumber = 2.6;
+  simpleNumber = -2;
+  simpleNumber = NaN;
+
+  // 3. Типизация логического типа - boolean
+  let isAdmin: boolean = true;
+  isAdmin = false;
+  isAdmin = userName !== "Bob";
+
+  // 4. Типизация неопределённого типа - undefined
+  let age: undefined = undefined;
+
+  // 5. Типизация пустого типа - null
+  let data: null = null;
+
+  // 6. Типизация большого числа - bigint
+  let bigNumber: bigint = 12344444444444444444444444444444444422222n;
+
+  // 7. Типизация символа - symbol
+  let uniq: symbol = Symbol("description");
+  // uniq = "234809fffffff2";
+
+  // 8.1 Типизация массив - указываем после названия массива тип данных, которые
+  // будут храниться в массиве, а затем ставим квадратные скобки
+  const colors: string[] = ["red", "blue", "green"];
+  // colors.push(65789);
+
+  // Для типизация массива постоянной длины(можно с разными типами) можно
+  // использовать кортеж (tupel)
+  const colorCode: [string, number] = ["blue", 673];
+
+  // 8.2 Типизация функций
+  // Если функция ничего не возвращает
+  const showMessage = (): void => {
+    console.log("Hello");
+  };
+
+  // Если функция возвращает значение, тогда просто прописываем его тип
+  const getSum = (): number => {
+    const sum: number = 2 + 7;
+    return sum;
+  };
+
+  // Типизация параметров функции
+  const getUserData = (firstName: string, age: number): string => {
+    return `${firstName} - ${age} y.o.`;
+  };
+
+  // 9. Автоматическое опеределение типов
+  let name = "Bob";
+  //   name = true;
+
+  const number = [4, 5, 6];
+//   number.push("89");
+
+// 10. Union типы
+let color: string | number = 78;
+color = "blue";
+
+const example: (string | number)[] = [34, "green", 67];
+
+  return (
+    <div className="lesson06-wrapper">
+      <div>Lesson06 - TypeScript</div>
+      {getUserData("Tom", 45)}
+    </div>
+  );
+}
+
+export default Lesson06;
